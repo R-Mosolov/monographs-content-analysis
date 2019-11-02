d736f5f71d80e20a8ce2cf54454e5db3877180f3
R-Mosolov <r.v.mosolov@ya.ru>
2019-11-03 01:05:17 +0300

    Returned data from Python move to new table

--- app.py
+++ app.py
@@ -3,33 +3,43 @@
 import nltk
 from flask import Flask, render_template, request
 
-from nltk.tokenize import sent_tokenize
-text="""Hello Mr. Smith, how are you doing today? The weather is great, and city is awesome.
-The sky is pinkish-blue. You shouldn't eat cardboard"""
-tokenized_text=sent_tokenize(text)
-# print(tokenized_text)
-
-from nltk.tokenize import word_tokenize
-tokenized_word=word_tokenize(text)
-# print(tokenized_word)
-
-from nltk.probability import FreqDist
-fdist = FreqDist(tokenized_word)
-# print(fdist)
-
-calculating_result=fdist.most_common(2)
-# print(result)
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/', methods=['POST'])
 def getvalue():
-    result = request.form['data-field']
-    return render_template('result.html', r=result)
+    from nltk.tokenize import sent_tokenize
+    text = request.form['data-field']
+    tokenized_text = sent_tokenize(text)
+    # print(tokenized_text)
+
+    from nltk.tokenize import word_tokenize
+    tokenized_word = word_tokenize(text)
+    # print(tokenized_word)
+
+    from nltk.probability import FreqDist
+    fdist = FreqDist(tokenized_word)
+    # print(fdist)
+
+    calculating_result = fdist.most_common(5)
+    # print(calculating_result[0])
+
+    import matplotlib.pyplot as plt
+    # fdist.plot(30, cumulative=False)
+    # plt.show()
+
+    result1 = calculating_result[0]
+    result2 = calculating_result[1]
+    result3 = calculating_result[2]
+    result4 = calculating_result[3]
+    result5 = calculating_result[4]
+    return render_template('result.html', r1=result1, r2=result2, r3=result3, r4=result4,  r5=result5)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
