899e7e9966018a60c91e4ff8113ffaee7c7dbf0b
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-05 11:06:51 +0300

    An universal path for searching dataset items was created

--- private_apps/AI_fiction_or_scientific/modules/preparation/centralize_texts.py
+++ private_apps/AI_fiction_or_scientific/modules/preparation/centralize_texts.py
@@ -13,18 +13,19 @@ def run():
         return paths_arr
 
     all_texts = ''
+    universal_path = '../../../texts-datasets/AI_fiction_or_scientific/dataset/'
 
-    defoe = create_full_path('dataset/fiction/foreign/Defoe/')
-    dikkens = create_full_path('dataset/fiction/foreign/Dikkens/')
-    vern = create_full_path('dataset/fiction/foreign/Vern/')
+    defoe = create_full_path(universal_path + 'fiction/russian/Defoe/')
+    dikkens = create_full_path(universal_path + 'fiction/foreign/Dikkens/')
+    vern = create_full_path(universal_path + 'fiction/foreign/Vern/')
 
-    bulgakov = create_full_path('dataset/fiction/russian/Bulgakov/')
-    chehov = create_full_path('dataset/fiction/russian/Chehov/')
-    dostoevskiy = create_full_path('dataset/fiction/russian/Dostoevskiy/')
-    gogol = create_full_path('dataset/fiction/russian/Gogol/')
-    pushkin = create_full_path('dataset/fiction/russian/Pushkin/')
-    tolstoy = create_full_path('dataset/fiction/russian/Tolstoy/')
-    turgenev = create_full_path('dataset/fiction/russian/Turgenev/')
+    bulgakov = create_full_path(universal_path + 'fiction/russian/Bulgakov/')
+    chehov = create_full_path(universal_path + 'fiction/russian/Chehov/')
+    dostoevskiy = create_full_path(universal_path + 'fiction/russian/Dostoevskiy/')
+    gogol = create_full_path(universal_path + 'fiction/russian/Gogol/')
+    pushkin = create_full_path(universal_path + 'fiction/russian/Pushkin/')
+    tolstoy = create_full_path(universal_path + 'fiction/russian/Tolstoy/')
+    turgenev = create_full_path(universal_path + 'fiction/russian/Turgenev/')
 
     texts_arr = (defoe + dikkens + vern) + (bulgakov + chehov + dostoevskiy + gogol + pushkin + tolstoy + turgenev)
 
