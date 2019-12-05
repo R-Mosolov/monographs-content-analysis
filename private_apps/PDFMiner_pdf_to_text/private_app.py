25d276654ecdd12e5b2f2efb1bf7f15abedfe1da
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-05 11:12:51 +0300

    The path for searching dataset items in PDFMiner was changed

--- private_apps/PDFMiner_pdf_to_text/private_app.py
+++ private_apps/PDFMiner_pdf_to_text/private_app.py
@@ -1,5 +1,5 @@
 import sys
 import pdfminer.high_level
 
-with open('pdf/examples/RU_Kant_I._Sochineniya_V_6_Tomah_T._1.pdf', 'rb') as file:
+with open('../../../texts-datasets/PDFMiner_pdf_to_text/pdf/examples/RU_Kant_I._Sochineniya_V_6_Tomah_T._1.pdf', 'rb') as file:
     pdfminer.high_level.extract_text_to_fp(file, sys.stdout)
