b1b941c1921d80cf19b55c7ff50e4e61058fe3ba
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-07 22:58:14 +0300

    Add the stemmer, which prepares words to general stems

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-# For starting code type in terminal: % pip install nltk
-
-import nltk
 from flask import Flask, render_template, request
+
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from nltk.stem.snowball import RussianStemmer
 
 app = Flask(__name__)
 
@@ -13,45 +15,37 @@ def index():
 
 @app.route('/', methods=['POST'])
 def getvalue():
+
     # 1. Add text
-
-    from nltk.tokenize import sent_tokenize
     text = request.form['data-field'].lower()
-    tokenized_text = sent_tokenize(text)
-
-    from nltk.tokenize import word_tokenize
-    tokenized_word = word_tokenize(text)
 
     # 2. Delete punctuation signs
-
-    from nltk.tokenize import RegexpTokenizer
     tokenizer = RegexpTokenizer(r'\w+')
     text_without_punctuation_signs = tokenizer.tokenize(text)
 
     # 3. Delete stop-words
-
-    from nltk.corpus import stopwords
     stop_words = stopwords.words('russian')
+    filtered_sent = []
     stop_words.extend(['не', 'это', 'что', 'именно', 'эта', 'лишь', 'очень', 'либо', 'или', 'ru', 'которые',
                        'которая', 'который', 'ибо', 'см', 'n', 'например', 'является', '1', '2', '3', '4', '5', '6',
                        '7', '8', '9', '0', '', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
                        'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'])
-
-    filtered_sent = []
-
     for w in text_without_punctuation_signs:
         if w not in stop_words:
             filtered_sent.append(w)
 
-    # 4. Calculate TOP-words
+    # 4. Prepare words to general stems
+    stemmer = RussianStemmer('ru')
+    stemmed_words = []
+    for word in filtered_sent:
+        stemmed_word = stemmer.stem(word)
+        stemmed_words.append(stemmed_word)
 
-    from nltk.probability import FreqDist
-    fdist = FreqDist(filtered_sent)
-
+    # 5. Calculate TOP-words
+    fdist = FreqDist(stemmed_words)
     result = fdist.most_common(10)
 
-    # 5. Return result in HTML
-
+    # 6. Return result in HTML
     word1 = result[0][0]
     word2 = result[1][0]
     word3 = result[2][0]
