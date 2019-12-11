8a15f4d95f042330982dc22c33c90483e182797f
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-11 07:33:22 +0300

    Rename initialization files in directories. Delete drafts

--- private_apps/AI_fiction_or_scientific/drafts/draft_1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from __future__ import print_function
-import os
-
-path = 'dataset/fiction/foreign/Vern'
-
-files = os.listdir(path)
-for name in files:
-    print(name)
--- private_apps/AI_fiction_or_scientific/drafts/draft_5.py
+++
@@ -1,4 +0,0 @@
-import statistics
-
-arr = [1, 1, 2, 4]
-print(statistics.median(arr))
